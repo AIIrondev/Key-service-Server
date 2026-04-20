5f27dcd46d548cd81f57ae7c34a8354ed1d8cfb5
Aiirondev <iron.ai.dev@gmail.com>
2026-04-20 13:56:24 +0200

    Refactor instace class to include type hints and improve method documentation

--- Website/server-steering.py
+++ Website/server-steering.py
@@ -1,10 +1,31 @@
 import os
 
 
-class instace():
+class instace:
     def __init__(self):
         pass
 
-    def new():
+    def new(name: str) -> bool:
+        """
+        Generates a new instance with the subdomain [name].invario.eu
+        
+        Input:
+        - name -> String
+
+        Output:
+        bool if the initiation works (True: generation worked; False: didnt work)
+        """
         pass
-    
+
+class ussage:
+    def __init__(self):
+        pass
+
+    def ram():
+        pass
+
+    def cpu():
+        pass
+
+    def storage():
+        pass
