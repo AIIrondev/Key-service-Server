4f8c262df1b4c86bdb8adc5236c32ff951456dcd
AIIrondev <iron.ai.dev@gmail.com>
2026-04-20 15:00:39 +0200

    Fix documentation formatting in instace class and add placeholder docstring for ram method

--- Website/server-steering.py
+++ Website/server-steering.py
@@ -13,7 +13,7 @@ class instace:
         - name -> String
 
         Output:
-        bool if the initiation works (True: generation worked; False: didnt work)
+        - bool if the initiation works (True: generation worked; False: didnt work)
         """
         pass
 
@@ -22,6 +22,7 @@ class ussage:
         pass
 
     def ram():
+        """"""
         pass
 
     def cpu():
